05c45ab4d9607f6e214d148fdebfa633880f8eec
Mohd Saad <developedbysaad@gmail.com>
2023-07-30 16:06:31 +0530

    redefining route apps

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,28 +1,13 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import "./App.css";
-import { ProtectedRoute } from "./ProtectedRoute";
-import NotFound from "./NotFound";
-import Signup from "./pages/signup";
-import Signin from "./pages/signin";
-import Dashboard from "./pages/dashboard";
+import { RouterProvider } from "react-router-dom";
 
-function App() {
+import router from "./routes"
+
+const App = () => {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Signup />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route
-          path="/dashboard"
-          element={<ProtectedRoute element={<Dashboard />} />}
-        />
-        <Route path="/notfound" element={<NotFound />} />
-        <Route path="*" element={<Navigate to="/notfound" />} />
-      </Routes>
+      <RouterProvider router={router} />
     </div>
   );
 }
-
 export default App;
